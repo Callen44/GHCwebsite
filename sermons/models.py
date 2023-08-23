8264f834ab2833d558a60b7978d3eaf4062f5a83
Caleb Allen <caleballen44@outlook.com>
2023-08-23 17:43:07 +0000

    do some work on sermons models

--- sermons/models.py
+++ sermons/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class apikeys(models.Model):
     key = models.CharField(default="")
+
+class video(models.Model):
+    date = models.TimeField()
+    vid = models.CharField()
+    
