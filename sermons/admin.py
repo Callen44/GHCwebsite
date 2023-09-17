558f15cb385b556a6430654e353f6c931283756c
callen44 <caleballen44@outlook.com>
2023-09-17 15:25:48 -0500

    website database now autofills with all videos

--- sermons/admin.py
+++ sermons/admin.py
@@ -4,4 +4,3 @@ from .models import *
 # Register your models here.
 admin.site.register(apikey)
 admin.site.register(video)
-admin.site.register(page)
--- sermons/reset_db_file.py
+++ sermons/reset_db_file.py
@@ -1,8 +1,85 @@
 import urllib.request
 import json
-from .models import apikey, video
+from .models import apikey
+from .models import video as video_data
+from googleapiclient.discovery import build
+from django.utils.timezone import make_aware
+import re  # Import the regular expressions module
 
 def reset_db():
+    prep()
+    
+    # Create a service object for interacting with the YouTube API
+    youtube = build('youtube', 'v3', developerKey=key)
+
+    # Make a request to retrieve information about the channel
+    response = youtube.channels().list(
+        part='contentDetails',
+        id=channelID
+    ).execute()
+
+    # Extract the playlist ID for the channel's uploaded videos
+    playlist_id = response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    # Retrieve all videos from the playlist
+    videos = []
+    next_page_token = None
+
+    while True:
+        # Make a request to retrieve videos from the playlist
+        response = youtube.playlistItems().list(
+            part='snippet',
+            playlistId=playlist_id,
+            maxResults=50,
+            pageToken=next_page_token
+        ).execute()
+
+        # Add the retrieved videos to our list
+        videos.extend(response['items'])
+
+        # Check if there are more pages of results
+        next_page_token = response.get('nextPageToken')
+
+        if not next_page_token:
+            break
+
+    # Process the list of videos and save them to the database
+    for video in videos:
+        title = video['snippet']['title']
+        video_id = video['snippet']['resourceId']['videoId']
+        thumbnail_url = video['snippet']['thumbnails']['default']['url']
+        youtube_url = f'https://www.youtube.com/watch?v={video_id}'
+        publish_date = video['snippet']['publishedAt']
+
+        # Extract the date part from publish_date and format it as 'YYYY-MM-DD'
+        publish_date = publish_date.split('T')[0]
+
+        # Use regular expressions to check if "bible" and "study" are together in the title
+        if re.search(r'\b(?:bible\s*study|study\s*bible)\b', title, re.IGNORECASE):
+            bible_study = True
+        else:
+            bible_study = False
+
+        # Create an instance of the video model and save it to the database
+        new_video = video_data(
+            official_name=title,
+            date=publish_date,
+            thumbnail_url=thumbnail_url,
+            biblestudy=bible_study,  # Set the biblestudy field based on the pattern match
+            youtubeid=video_id
+        )
+        new_video.save()
+
+        print(f'Saved video: {title}, URL: {youtube_url}, Publish Date: {publish_date}, Bible Study: {bible_study}')
+
+# The rest of your code remains the same
+
+
+        
+
+def prep():
+    global key, channelID
+
     print("reset_db_log: starting")
 
     # check if updater is on
@@ -30,18 +107,9 @@ def reset_db():
         print("log: key is "+key)
     except:
         raise KeyError('ERROR: youtube API key required!')
-
-    # a valid key is provided, continuing to search youtube for all
-    # videos made by the youtube channel @GraceHeritageChurch
-
+    
     channelID = 'UCoMOgaiSCktWEk2CZ7e-aLQ' # this is the @GraceHeritageChurch channel id
-    request_url = "https://www.googleapis.com/youtube/v3/search?key="+key+"&channelId="+channelID+"&part=snippet,id&order=date"
-
-    print(request_url)
-    print("log: making request - " + request_url)
-    with urllib.request.urlopen(request_url) as response:
-        data = json.loads(response.read().decode())
-        print(data)
+    
 
 if __name__ == "__main__":
     reset_db()
