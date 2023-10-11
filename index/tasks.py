9f420de3178d57d69320e881cc2550b404b97482
callen44 <caleballen44@outlook.com>
2023-10-11 16:45:05 -0500

    Done!

--- index/tasks.py
+++ index/tasks.py
@@ -11,16 +11,17 @@ def email_pastors_new_message(message_id):
 
     # find the message in the database so that the pastors can read it from their email
     message = models.contact_request.objects.get(id=message_id)
-    email_message = """A new Contact Us request was recieved!
+    email_message = """
+    A new Contact Us request was recieved!
     
-    info:
+    Note: don't reply to this email nothing will happen, contact the senders email below.
+
+    info,
     name: """ + str(message.name) + """
     email: """ + str(message.email)+"""
     message:
     
     """ + str(message.message)
-
-    print (email_message)
     
     emails = models.pastor_email.objects.all()
     for email in emails:
@@ -28,5 +29,7 @@ def email_pastors_new_message(message_id):
 
         send_mail(
             subject="New Contact Us request recieved!",
-            message=email_message
+            message=email_message,
+            recipient_list=[email],
+            from_email=None
         )
