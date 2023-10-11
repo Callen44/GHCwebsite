671ee992899eb58c3af083f48470f6e3fbbd0193
Caleb Allen <caleballen44@outlook.com>
2023-10-11 20:49:49 +0000

    add paramaters to the contact us task in views

--- index/views.py
+++ index/views.py
@@ -54,6 +54,6 @@ def contactus(request):
         pass
 
     # schedule a task for the pastors to recieve an email
-    email_pastors_new_message.apply_async(eta=timezone.now())
+    email_pastors_new_message.apply_async((new_contact_request.id),eta=timezone.now())
 
     return render(request,"contactus.html", {"Message":msg,"Success":success})
