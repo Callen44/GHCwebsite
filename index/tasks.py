e264bdeced36a8388da8be4da09d3ef50b19b8aa
Caleb Allen <caleballen44@outlook.com>
2023-10-11 20:50:27 +0000

    begin work on sending emails

--- index/tasks.py
+++ index/tasks.py
@@ -1,10 +1,25 @@
 # tasks for index app
 
 from celery import shared_task
+from . import models
+from django.core.mail import send_mail
 
 @shared_task
-def email_pastors_new_message():
+def email_pastors_new_message(message_id):
     # Send the pastors email notifying them of the new message
-    from django.core.mail import send_mail
     print('log: sending mail to pastors')
-    print('! this function dose not work !')
+
+    # find the message in the database so that the pastors can read it from their email
+    message = models.contact_request.objects.get(id=message_id)
+    email_message = """
+    A new Contact Us request was recieved 
+    """
+    
+    emails = models.pastor_email.objects.all()
+    for email in emails:
+        email = str(email.email)
+
+        send_mail(
+            subject="New Contact Us request recieved!",
+            message=email_message
+        )
