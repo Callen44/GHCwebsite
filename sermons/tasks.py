5c6785867c762fa630d83c33587048af793cca01
callen44 <caleballen44@outlook.com>
2023-10-18 20:31:05 -0500

    some work that needs to be tested later on sunday

--- sermons/tasks.py
+++ sermons/tasks.py
@@ -10,7 +10,10 @@ import os
 
 @shared_task
 def cel_reset_db():
-    reset_db()
+    try:
+        reset_db()
+    except:
+        print('log: error occurred when updating sermon database!!!!! scheduling for next sunday anyway')
     print('log: task complete, sermons updated, scheduling for next sunday')
 
     # Personal note, I know this should not be commited to the repo
@@ -21,6 +24,25 @@ def cel_reset_db():
     # figure out how long until 12:00 on the next sunday, and schedule the task
     now = datetime.now()
     next_sunday = now + timedelta(days=(6 - now.weekday()))
+
+    # in most cases this program will be run on sunday in which the value for next sunday will be less than 2
+    day = int(next_sunday.strftime('%d'))
+    
+    print("pre - " + str(day))
+    
+    # to make developemnt easier the weekday is kept outside of the task
+    weekday = now.weekday
+
+    if weekday == 6:
+        # it's probobly sunday
+        print(day)
+        print(now.weekday())
+
+        day = day + 7
+        next_sunday.replace(day=day)
+
+
     next_sunday = next_sunday.replace(hour=12, minute=0, second=0, microsecond=0)
+    print(f'log: task will be run on {next_sunday}')
     # Run the task on the next sunday
     cel_reset_db.apply_async(eta=next_sunday)
